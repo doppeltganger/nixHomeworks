080029679e6f42079165fbd26ff339648716899d
doppeltganger <stanislav.danylov@gmail.com>
2021-08-23 18:54:23 +0300

    files created

---
+++ index.js
@@ -0,0 +1,55 @@
+//first task
+const searchCandidatesByPhoneNumber = (inputArr, inputValue) => {
+  const regexValue = /[\+\(\)\-\s]/g
+  return inputArr.filter(element => element.phone.replace(regexValue, '').includes(inputValue.replace(regexValue, '')))
+}
+
+console.log(searchCandidatesByPhoneNumber(candidateArr, '43')) 
+console.log(searchCandidatesByPhoneNumber(candidateArr, '+1(869) 40'))  
+console.log(searchCandidatesByPhoneNumber(candidateArr, '+1 (869)408-39-82')) 
+  
+//second task
+const getCandidateById = (inputArr, inputId) => {
+  const outputValue = inputArr.find(element => element._id === String(inputId))
+  const formattedData = new Date(outputValue.registered).toLocaleDateString()
+  outputValue.registered = formattedData.replace(formattedData.slice(6, 8), '')
+  
+  return outputValue
+}
+
+console.log(getCandidateById(candidateArr, '5e216bc9a6059760578aefa4'))
+
+//third task
+const sortCandidatesArr = (inputArr, inputSortArg) => {
+  const formattedBalance = inputValue => {
+    return Number.parseInt(inputValue.replace('$', ''))
+  }
+  
+  return [...inputArr].sort((a, b) => {
+    return (
+      (inputSortArg === 'ascendingOrder') ? formattedBalance(a.balance) - formattedBalance(b.balance) :
+      (inputSortArg === 'descendingOrder') ? formattedBalance(b.balance) - formattedBalance(a.balance) :
+      [...inputArr]
+    )
+  })
+}
+
+console.log(sortCandidatesArr(candidateArr, 'ascendingOrder'))
+console.log(sortCandidatesArr(candidateArr, 'descendingOrder'))
+console.log(sortCandidatesArr(candidateArr))
+
+//fourth task
+const getEyeColorMap = inputArr => {
+  const outputObjValueArr = []
+  inputArr.forEach(element => {
+    outputObjValueArr.push(element.eyeColor)
+  })
+
+  const outputObj = {}
+  for (let eyeColor in outputObjValueArr) {
+    outputObj[outputObjValueArr[eyeColor]] = inputArr.filter(element => outputObjValueArr[eyeColor] === element.eyeColor)
+  }
+
+  return outputObj
+}
+console.log(getEyeColorMap(candidateArr))
