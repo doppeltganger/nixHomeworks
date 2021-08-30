5fc2b6d0eada2849ea013a7bf7e87b8fd4e863e2
doppeltganger <stanislav.danylov@gmail.com>
2021-08-30 05:50:36 +0300

    files modified

--- index.js
+++ index.js
@@ -1,81 +1,75 @@
 //first task
 for (let i = 1; i <= 10; ++i) {
-  console.log((i % 3 == 0) ? `${i}:FizBuz` :
-  (i % 2 == 0) ? `${i}:Fiz` : `${i}:Buz`)
+  if (!(i % 3)) {
+    console.log(`${i}:FizBuz`) 
+  } else if (!(i % 2)) {
+    console.log(`${i}:Fiz`)
+  } else {
+    console.log(`${i}:Buz`)
+  }
 }
 
-//second task with while
-const calculateFactorial1 = value => {
-  let factorial = 1
-
-  while (value) factorial *= value--
-
-  console.log(factorial)
+//second task
+const calculateFactorial = number => {
+  let result = 1
+    return (fact = () => {
+      result *= number
+      return number < 2 ? result : fact (--number)
+    })()
 }
 
-calculateFactorial1(7)
-
-//second task with for
-const calculateFactorial2 = value => {
-  let factorial = 1
-
-  for (let i = value; i > 1; i--) factorial *= i
-
-  console.log(factorial)
-}
-
-calculateFactorial2(7)
+calculateFactorial(7)
 
 //third task
 const calculateTotalAmountOfReamPaper = (sheetsInReamPaper, consumptionPerWeek, weeksAmount) => {
-  let totalAmount
-
-  (consumptionPerWeek * weeksAmount % sheetsInReamPaper == 0) ? 
-    totalAmount = consumptionPerWeek * weeksAmount / sheetsInReamPaper :
-    totalAmount = (consumptionPerWeek * weeksAmount - (consumptionPerWeek * weeksAmount % sheetsInReamPaper)) / sheetsInReamPaper + 1
-
-  console.log(totalAmount) 
+  let totalAmount = consumptionPerWeek * weeksAmount / sheetsInReamPaper
+  
+  if (!(totalAmount % 1)) {
+    return totalAmount
+  }
+  return (totalAmount - totalAmount % 1) + 1 
 }
 
-calculateTotalAmountOfReamPaper(500, 1200, 8)
+console.log(calculateTotalAmountOfReamPaper(500, 1200, 8))
 
 //fourth task
 const calculateNumberOfPorchAndNumberOfFloor = (roomsOnFloor, floors, roomNumber) => {
-  let numberOfPorch
-  (roomNumber % (roomsOnFloor * floors) == 0) ?
-  numberOfPorch = roomNumber / (roomsOnFloor * floors) : 
-  numberOfPorch = (roomNumber - roomNumber % (roomsOnFloor * floors)) / (roomsOnFloor * floors) + 1 
+  calculateNumberOfPorch = () => {
+    let numberOfPorch = roomNumber / (roomsOnFloor * floors)
+    if (!(numberOfPorch % 1)) {
+      return numberOfPorch
+    } 
+    return (numberOfPorch - numberOfPorch % 1) + 1
+  } 
   
-  let numberOfFloor
-  ((roomNumber - (numberOfPorch - 1) * roomsOnFloor * floors) % roomsOnFloor == 0) ?
-  numberOfFloor = (roomNumber - (numberOfPorch - 1) * roomsOnFloor * floors) / roomsOnFloor :
-  numberOfFloor = (roomNumber - roomNumber % roomsOnFloor - (numberOfPorch - 1) * roomsOnFloor * floors) / roomsOnFloor + 1
+  calculatenumberOfFloor = () => {
+    let numberOfFloor = (roomNumber - (calculateNumberOfPorch() - 1) * roomsOnFloor * floors) / roomsOnFloor
+    if (!(numberOfFloor % 1)) {
+      return numberOfFloor
+    } 
+    return (numberOfFloor - numberOfFloor % 1) + 1
+  }
   
-  console.log(`Number of your porch is ${numberOfPorch}
-Number of your floor is ${numberOfFloor}`)
+  return `Number of your porch is ${calculateNumberOfPorch()} 
+Number of your floor is ${calculatenumberOfFloor()}`
 }
 
-calculateNumberOfPorchAndNumberOfFloor(3, 9, 456)
+console.log(calculateNumberOfPorchAndNumberOfFloor(5, 9, 456))
 
 //fifth task
 const getPyramid = medianNumber => {
-  let pyramid = ''
-  let hyphen = '-'
-  let hash = '#'
-
-  for (let i = 0; i < medianNumber; i++) {
-    for (let j = 1; j < medianNumber - i; j++) {
-      pyramid += hyphen
+  for (let i = 1; i <= medianNumber; i++) {
+    let pyramid = ''
+    for (let j = 1; j <= (2 * medianNumber - 1); j++) {  
+      if (j >= medianNumber + 1 - i && j <= medianNumber - 1 + i) {
+        pyramid += '#'
+      } else {
+        pyramid += '-'
+      }    
     }
-    for (let j = medianNumber - 2 * i; j <= medianNumber; j++) {
-      pyramid += hash
-    }
-    for (let j = 1; j < medianNumber - i; j++) {
-      pyramid += hyphen  
-    }
-    pyramid += '\n'
+    console.log(pyramid)
   }
-  console.log(pyramid)
 }
 
 getPyramid(8)
+
