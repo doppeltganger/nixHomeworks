ec4f5aa29433e59e11ca55f607e6485a2c35e369
doppeltganger <stanislav.danylov@gmail.com>
2021-09-06 09:09:55 +0300

    files created

--- index.js
+++ index.js
@@ -1,66 +1,150 @@
 //first task
-const searchCandidatesByPhoneNumber = (inputArr, inputValue) => {
-  const regexValue = /[\+\(\)\-\s]/g
-  return inputArr
-    .filter(element => element.phone
-      .replace(regexValue, '')
-      .includes(inputValue.replace(regexValue, ''))
-    )
-}
+const userNumber = Number(prompt('Enter your number'))
 
-console.log(searchCandidatesByPhoneNumber(candidateArr, '43')) 
-console.log(searchCandidatesByPhoneNumber(candidateArr, '+1(869) 40'))  
-console.log(searchCandidatesByPhoneNumber(candidateArr, '+1 (869)408-39-82')) 
+  for (let index = 1; index <= userNumber; index++) {
+    const inputs = document.createElement('input')
+
+    inputs.classList.add('input-item')
+    inputs.value = `Input ${index}`
+
+    if (index === userNumber) {
+      inputs.classList.add('margin-zero')
+    }
+
+    if (index % 2) {
+      document.querySelector('style').append(document.innerHTML = '.input-bg {background: yellow}')
+      inputs.classList.add('input-bg')
+    }
+
+    if (!(index % 3)) {
+      inputs.value = ''
+      inputs.placeholder = 'Some text'
+    }
+
+    document.querySelector('.button').before(inputs)
+  }
   
 //second task
-const getCandidateById = (inputArr, inputId) => {
-  const outputValue = inputArr.find(element => element._id === String(inputId))
-  const formattedData = new Date(outputValue.registered).toLocaleDateString()
-  outputValue.registered = formattedData.replace(formattedData.slice(6, 8), '')
-  
-  return outputValue
-}
+const startButton = document.createElement('button')
+startButton.innerText = 'Start'
 
-console.log(getCandidateById(candidateArr, '5e216bc9a6059760578aefa4'))
+const stopButton = document.createElement('button')
+stopButton.innerText = 'Stop'
+
+const currentTime = document.createElement('h2')
+currentTime.innerText = new Date().toLocaleTimeString()
+
+document.body.append(startButton, stopButton, currentTime)
+
+startButton.addEventListener('click', () => {
+  const timerId = setInterval(() => {
+    currentTime.innerText = new Date().toLocaleTimeString()
+  }, 1000)
+
+  stopButton.addEventListener('click', () => {
+      clearInterval(timerId)
+  })
+})
 
 //third task
-const sortCandidatesArr = (inputArr, inputSortArg) => {
-  const formattedBalance = inputValue => {
-    return Number.parseInt(inputValue.replace('$', ''))
-  }
+const wrap = document.createElement('div')
+wrap.id = 'wrap'
 
-  if (!inputSortArg) {
-    return inputArr
-  }
+const main = document.createElement('div')
+main.id = 'main'
 
-  else {
-    return [...inputArr].sort((a, b) => {
-      if (inputSortArg === 'ascendingOrder')  {
-        return formattedBalance(a.balance) - formattedBalance(b.balance)
-      }
-      else if (inputSortArg === 'descendingOrder') {
-        return formattedBalance(b.balance) - formattedBalance(a.balance)
-      }
-    })
-  }
+const footer = document.createElement('div')
+footer.id = 'footer'
+
+const changeButton = document.createElement('button')
+changeButton.innerText = 'Change'
+
+const footerHeader = document.createElement('h2')
+footerHeader.innerText = 'Footer'
+
+document.body.append(wrap)
+wrap.append(footer, main, changeButton)
+footer.append(footerHeader)
+
+for (let index = 1; index <= 3; index++) {
+  const paragraph = document.createElement('p')
+  paragraph.innerText = `Im ${index} paragraph`
+  main.append(paragraph)
 }
 
-console.log(sortCandidatesArr(candidateArr, 'ascendingOrder'))
-console.log(sortCandidatesArr(candidateArr, 'descendingOrder'))
-console.log(sortCandidatesArr(candidateArr))
+changeButton.addEventListener('click', () => {
+  main.lastChild.setAttribute('style', 'background: red')
+  changeButton.before(footer)
+})
+
+//Если дефолтная вёрстка без кнопки
+// const changeLastParagraphColor = () => {
+//   document.querySelector('#main').lastChild.setAttribute('style', 'background: red')
+// }
+
+// const changeMainAndFooterPositions = () => {
+//   document.querySelector('#wrap').append(document.querySelector('#footer'))
+// }
+
+// changeLastParagraphColor()
+// changeMainAndFooterPositions()
 
 //fourth task
-const getEyeColorMap = inputArr => {
-  const outputObjValueArr = []
-  inputArr.forEach(element => {
-    outputObjValueArr.push(element.eyeColor)
-  })
+const ingredients = {
+  'cocoa': ['cocoa powder', 'milk', 'sugar'],
+  'cappuccino': ['milk', 'coffee'], 
+  'smoothie': ['banana', 'orange', 'sugar'],
+  'matcha frappe': ['matcha', 'milk', 'ice']
+}
 
-  const outputObj = {}
-  for (let eyeColor in outputObjValueArr) {
-    outputObj[outputObjValueArr[eyeColor]] = inputArr.filter(element => outputObjValueArr[eyeColor] === element.eyeColor)
+const menuHeader = document.createElement('h2')
+menuHeader.innerText = 'Menu'
+
+const menuList = document.createElement('ul')
+
+document.querySelector('style').append(document.innerHTML = '.closeList {display: none} .openList {display: block}')
+
+for (let key in ingredients) {
+  const menuItem = document.createElement('li')
+  menuItem.innerText = key
+  menuItem.style = `cursor: pointer`
+  menuList.append(menuItem)
+
+  const itemIngredients = document.createElement('ol')
+  itemIngredients.classList.add('closeList')
+  menuItem.append(itemIngredients)
+
+  ingredients[key].forEach(element => {
+    const itemIngredientsList = document.createElement('li')
+    itemIngredientsList.innerText = element
+    itemIngredients.append(itemIngredientsList)
+  }) 
+}
+
+document.body.append(menuHeader, menuList)
+
+const createList = () => {
+  let dropDownMenuItem
+
+  const openList = target => {
+    if (!dropDownMenuItem) {
+      dropDownMenuItem = target
+      dropDownMenuItem.classList.add('openList')
+    }
+    else {
+      if (dropDownMenuItem === target) {
+        dropDownMenuItem.classList.remove('openList')
+        return dropDownMenuItem = null
+      } else {
+        dropDownMenuItem.classList.remove('openList')
+        dropDownMenuItem = target.classList.add('openList')
+      }
+    }
   }
 
-  return outputObj
+  menuList.addEventListener('click', event => {
+    openList(event.target.childNodes[1])
+  })
 }
-console.log(getEyeColorMap(candidateArr))
+
+createList()
